63529847d0774d869936df0936a0c83cb45869fa
chiapeipan <revlis7@gmail.com>
2012-11-22 00:06:34 +0800

    remove base.js in user info page

--- ts/application/views/user/info.php
+++ ts/application/views/user/info.php
@@ -17,5 +17,4 @@
 .style_row_proj0 tr{background-color:#FFFFFF;}
 .style_row_proj1 tr{background-color:#E0E8F0;}
 </style>
-<script type="text/javascript" src="<?php echo base_url('misc/ts_base.js'); ?>"></script>
 <script type="text/javascript" src="<?php echo base_url('misc/ts_self.js'); ?>"></script>

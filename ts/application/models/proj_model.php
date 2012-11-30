29c2a4b3c707a0c34581146836b50dc4810b9c7e
mousefirst <mousefirst@gmail.com>
2012-11-30 15:20:26 +0800

    增加获取项目数据的过滤条件

--- ts/application/models/proj_model.php
+++ ts/application/models/proj_model.php
@@ -268,6 +268,7 @@ class Proj_model extends CI_Model {
 	function get_all_proj_detail() {
 		$this->db->select('proj.id as proj_id, proj.category, proj.sub_category, proj.issue, proj.name, proj.flow_of_fund, proj.highlights, proj.month, proj.scale, proj.cycle, proj.profit_property, proj.manager, proj.contract, proj.remark, proj.found, proj_detail.id as proj_detail_id, proj_detail.total_share, proj_detail.status, proj_detail.exclusive, proj_detail.grade, proj_detail.amount, proj_detail.profit, proj_detail.commission_b_tax, proj_detail.commission_a_tax, proj_detail.inner_commission, proj_detail.outer_commission, proj_detail.pay, proj_detail.paid, proj_detail.quota, proj_detail.quota_paid, proj_detail.quota_remain, proj_detail.main_channel, proj_detail.channel_company, proj_detail.channel_contact, proj_detail.billing_company, proj_detail.manager_remark');
 		$this->db->from('proj');
+		$this->db->where('proj_detail.status <> "结束"');
 		$this->db->join('proj_detail', 'proj_detail.proj_id = proj.id', 'left');
 		$this->db->order_by('proj.id', 'asc');
 		$query = $this->db->get();

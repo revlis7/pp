0c653647030acca3005b181c25496d73593e0f4a
revlis7 <silver47@yeah.net>
2013-06-11 00:59:22 +0800

    remove month in proj api

--- ts/application/controllers/proj.php
+++ ts/application/controllers/proj.php
@@ -132,44 +132,23 @@ class Proj extends Auth_Controller {
 		if(!$this->has_privilege()) {
 			$this->json->output(array('success' => false, 'm' => '您没有使用该功能的权限'));
 		}
-		//$total_share = $this->input->post('total_share', true);
-		//$status = $this->input->post('status', true);
-		//$exclusive = $this->input->post('exclusive', true);
-		//$grade = $this->input->post('grade', true);
 		$category = $this->input->post('category', true);
 		$sub_category = $this->input->post('sub_category', true);
 		$issue = $this->input->post('issue', true);
 		$name = $this->input->post('name', true);
 		$flow_of_fund = $this->input->post('flow_of_fund', true);
 		$highlights = $this->input->post('highlights', true);
-		$month = $this->input->post('month', true);
 		$scale = $this->input->post('scale', true);
 		$cycle = $this->input->post('cycle', true);
-		//$amount = $this->input->post('amount', true);
 		$profit_property = $this->input->post('profit_property', true);
-		//$profit = $this->input->post('profit', true);
 		$manager = $this->input->post('manager', true);
 		$contract = $this->input->post('contract', true);
 		$remark = $this->input->post('remark', true);
 		$pay_account = $this->input->post('pay_account', true);
 		$countdown = $this->input->post('countdown', true);
-		//$commission_b_tax = $this->input->post('commission_b_tax', true);
-		//$commission_a_tax = $this->input->post('commission_a_tax', true);
-		//$inner_commission = $this->input->post('inner_commission', true);
-		//$outer_commission = $this->input->post('outer_commission', true);
-		//$pay = $this->input->post('pay', true);
-		//$paid = $this->input->post('paid', true);
 		$found = $this->input->post('found', true);
-		//$quota = $this->input->post('quota', true);
-		//$quota_paid = $this->input->post('quota_paid', true);
-		//$quota_remain = $this->input->post('quota_remain', true);
-		//$main_channel = $this->input->post('main_channel', true);
-		//$channel_company = $this->input->post('channel_company', true);
-		//$channel_contact = $this->input->post('channel_contact', true);
-		//$billing_company = $this->input->post('billing_company', true);
-		//$manager_remark = $this->input->post('manager_remark', true);
-		
-		$proj_id = $this->Proj_model->create_proj($category, $sub_category, $issue, $name, $flow_of_fund, $highlights, $month, $scale, $cycle, $profit_property, $manager, $contract, $remark, $pay_account, $countdown, $found, element('loginname', $this->session->userdata('user')));
+
+		$proj_id = $this->Proj_model->create_proj($category, $sub_category, $issue, $name, $flow_of_fund, $highlights, $scale, $cycle, $profit_property, $manager, $contract, $remark, $pay_account, $countdown, $found, element('loginname', $this->session->userdata('user')));
 		if($proj_id === false) {
 			$this->json->output(array('success' => false, 'm' => '添加数据失败'));
 		}
@@ -192,7 +171,6 @@ class Proj extends Auth_Controller {
 		$name = $this->input->post('name', true);
 		$flow_of_fund = $this->input->post('flow_of_fund', true);
 		$highlights = $this->input->post('highlights', true);
-		$month = $this->input->post('month', true);
 		$scale = $this->input->post('scale', true);
 		$cycle = $this->input->post('cycle', true);
 		$profit_property = $this->input->post('profit_property', true);
@@ -217,7 +195,7 @@ class Proj extends Auth_Controller {
 			$this->User_model->operation_history(element('loginname', $this->session->userdata('user')), $this->get_user_info('realname').'将['.$proj->issue.']的项目：['.$proj->name.']的备注修改为［'.$remark.'］');
 		}
 		
-		$proj_id = $this->Proj_model->update_proj($proj_id, $category, $sub_category, $issue, $name, $flow_of_fund, $highlights, $month, $scale, $cycle, $profit_property, $manager, $contract, $remark, $pay_account, $countdown, $found, element('loginname', $this->session->userdata('user')));
+		$proj_id = $this->Proj_model->update_proj($proj_id, $category, $sub_category, $issue, $name, $flow_of_fund, $highlights, $scale, $cycle, $profit_property, $manager, $contract, $remark, $pay_account, $countdown, $found, element('loginname', $this->session->userdata('user')));
 		if($proj_id === false) {
 			$this->json->output(array('success' => false, 'm' => '修改数据失败'));
 		}

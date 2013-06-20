c774b2e74e34d6d1341d2d5b8791ac9d5bcd2c2a
Patrick <mousefirst@gmail.com>
2013-06-20 18:36:49 +0800

    ExtJS library update and new proj creatation test

--- ts/application/views/default.php
+++ ts/application/views/default.php
@@ -3,7 +3,7 @@
 <head>
 <meta charset="utf-8">
 <title><?php echo $this->utility->page_title(); ?></title>
-<link rel="stylesheet" type="text/css" href="<?php echo base_url('misc/resources/css/ext-all-gray.css'); ?>" />
+<link rel="stylesheet" type="text/css" href="<?php echo base_url('misc/resources/css/ext-all-neptune.css'); ?>" />
 <link rel="stylesheet" type="text/css" href="<?php echo base_url('misc/ux/grid/css/GridFilters.css'); ?>" />
 <link rel="stylesheet" type="text/css" href="<?php echo base_url('misc/ux/grid/css/RangeMenu.css'); ?>" />
 <script type="text/javascript" src="<?php echo base_url('misc/ext-all.js'); ?>"></script>

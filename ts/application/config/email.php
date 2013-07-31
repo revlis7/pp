c29ef0ba50ce2c6f0c11e7ced4b67e1db8bb2b17
Patrick <mousefirst@gmail.com>
2013-07-31 17:00:00 +0800

    补充更新

--- ts/application/config/email.php
+++
@@ -1,7 +0,0 @@
-﻿<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-
-$config['protocol'] = 'smtp';
-$config['smtp_host'] = 'smtp.ym.163.com';
-$config['smtp_user'] = 'notifications@firstshin.com';
-$config['smtp_pass'] = '23y23qBlfx';
-$config['mailtype'] = 'html';

5396bfa4d1902b6654311dda251b4f5233507e39
Patrick <mousefirst@gmail.com>
2013-08-14 18:28:59 +0800

    格式小调整

--- ts/application/views/default.php
+++ ts/application/views/default.php
@@ -14,21 +14,23 @@
     font-weight: 200;
     padding: 8px 15px;
     text-shadow: 0 1px 0 #fff;
+    font-family:Î¢ÈíÑÅºÚ,ºÚÌå,sans-serif;
 }
 .app-header2 {
-    color: #596F8F;
-    font-size: 16px;
+    color: #294F6F;
+    font-size: 20px;
     font-weight: 200;
     padding: 8px 15px;
     text-shadow: 0 1px 0 #fff;
+    font-family:Î¢ÈíÑÅºÚ,ºÚÌå,sans-serif;
 }
   .style_row_proj0 tr{background-color:#FFFFFF !important;cursor: pointer;color: #2f4b52}
   .style_row_proj1 tr{background-color:#F9EEF2 !important;cursor: pointer;color: #2f4b52}
-  .x-grid-row {height:28px;vertical-align:top}
+  .x-grid-row {height:26px;vertical-align:top}
   .x-grid-cell {height:26px;vertical-align:top}
   .x-grid-cell-inner {height:26px;font-size:12px;padding:1px 6px 1px 6px;line-height:26px}
   .r_ex_td_pre {border-collapse:collapse;width:75px;padding:8px;border:1px solid #C1DAD7;background: #fff;color: #3f5b62;font-size:12px}
-  .r_ex_td_main {border-collapse:collapse;width:400px;padding:8px;border:1px solid #C1DAD7;background: #fff;color: #3f5b62;font-size:12px}
+  .r_ex_td_main {border-collapse:collapse;width:460px;padding:8px;border:1px solid #C1DAD7;background: #fff;color: #3f5b62;font-size:12px}
   .r_ex_td_pre pre{margin:0;overflow:auto;white-space: pre-wrap; white-space: -moz-pre-wrap; white-space: -pre-wrap; white-space: -o-pre-wrap;word-wrap:break-word;}
   .r_ex_td_main pre{margin:0;overflow:auto;white-space: pre-wrap; white-space: -moz-pre-wrap; white-space: -pre-wrap; white-space: -o-pre-wrap;word-wrap:break-word;}
 </style>
